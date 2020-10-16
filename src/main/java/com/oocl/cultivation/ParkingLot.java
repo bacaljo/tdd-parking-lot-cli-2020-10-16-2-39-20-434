c9d8c2a9c775867c489032d86dc9f8d930ac0c96
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-16 14:08:21 +0800

    [Parking Lot] [John Emmanuel] Feature: ParkingBoy returns the car associated with the provided ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,4 +1,23 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private final Map<ParkingTicket, Car> parkingTicketCarMap;
+
+    public ParkingLot() {
+        parkingTicketCarMap = new HashMap<>();
+    }
+
+    public ParkingTicket park(Car car) {
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkingTicketCarMap.put(parkingTicket, car);
+
+        return parkingTicket;
+    }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        return parkingTicketCarMap.get(parkingTicket);
+    }
 }
