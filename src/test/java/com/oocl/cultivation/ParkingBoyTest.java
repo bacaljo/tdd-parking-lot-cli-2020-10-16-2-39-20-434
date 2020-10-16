5347fed1fdc7d05356d526a64afaf08244c49bdc
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-16 14:04:22 +0800

    [Parking Lot] [John Emmanuel] Test: Test if parking boy fetches the correct car when given the correct ticket

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class ParkingBoyTest {
     @Test
@@ -17,4 +18,19 @@ class ParkingBoyTest {
         // THEN
         assertNotNull(parkingTicket);
     }
+
+    @Test
+    public void should_return_the_correct_car_when_parking_boy_fetch_given_the_correct_ticket() {
+        // GIVEN
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        ParkingTicket parkingTicket = parkingBoy.park(car);
+
+        // WHEN
+        Car fetchedCar = parkingBoy.fetch(parkingTicket);
+
+        // THEN
+        assertSame(fetchedCar, car);
+    }
+
 }
