a16de3c96386f01119817b5284c03e62b12ea2fb
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-16 15:07:39 +0800

    [Parking Lot] [John Emmanuel] Test: Test parking lot using the same test cases from parking boy

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -3,6 +3,9 @@ package com.oocl.cultivation;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 class ParkingLotTest {
     @Test
@@ -29,5 +32,112 @@ class ParkingLotTest {
 
         // THEN
         assertEquals(2, parkingLot.getCapacity());
+    }
+
+    @Test
+    public void should_return_a_parking_ticket_when_parking_lot_park_given_a_car() {
+        // GIVEN
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        // WHEN
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        // THEN
+        assertNotNull(parkingTicket);
+    }
+
+    @Test
+    public void should_return_the_correct_car_when_parking_lot_fetch_given_the_correct_ticket() {
+        // GIVEN
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        // WHEN
+        Car fetchedCar = parkingLot.fetch(parkingTicket);
+
+        // THEN
+        assertSame(fetchedCar, car);
+    }
+
+    @Test
+    public void should_return_the_correct_cars_when_parking_lot_fetch_given_two_tickets() {
+        // GIVEN
+        ParkingLot parkingLot = new ParkingLot();
+        Car car1 = new Car();
+        Car car2 = new Car();
+
+        ParkingTicket parkingTicket1 = parkingLot.park(car1);
+        ParkingTicket parkingTicket2 = parkingLot.park(car2);
+
+        // WHEN
+        Car fetchedCar1 = parkingLot.fetch(parkingTicket1);
+        Car fetchedCar2 = parkingLot.fetch(parkingTicket2);
+
+        // THEN
+        assertSame(fetchedCar1, car1);
+        assertSame(fetchedCar2, car2);
+    }
+
+    @Test
+    public void should_return_null_when_parking_lot_fetch_given_unassociated_ticket() {
+        // GIVEN
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        parkingLot.park(car);
+
+        ParkingTicket fakeParkingTicket = new ParkingTicket();
+
+        // WHEN
+        Car fetchedCar = parkingLot.fetch(fakeParkingTicket);
+
+        // THEN
+        assertNull(fetchedCar);
+    }
+
+    @Test
+    public void should_return_null_when_parking_lot_fetch_given_null_ticket() {
+        // GIVEN
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        parkingLot.park(car);
+
+        ParkingTicket nullParkingTicket = null;
+
+        // WHEN
+        Car fetchedCar = parkingLot.fetch(nullParkingTicket);
+
+        // THEN
+        assertNull(fetchedCar);
+    }
+
+    @Test
+    public void should_return_null_when_parking_lot_fetch_given_an_already_used_ticket() {
+        // GIVEN
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        // WHEN
+        parkingLot.fetch(parkingTicket);
+        Car fetchedCar = parkingLot.fetch(parkingTicket);
+
+        // THEN
+        assertNull(fetchedCar);
+    }
+
+    @Test
+    public void should_return_null_when_parking_lot_park_given_capacity_1_and_a_parked_car() {
+        // GIVEN
+        int capacity = 1;
+        ParkingLot parkingLot = new ParkingLot(capacity);
+        parkingLot.park(new Car());
+
+        // WHEN
+        ParkingTicket parkingTicket = parkingLot.park(new Car());
+
+        // THEN
+        assertNull(parkingTicket);
     }
 }
