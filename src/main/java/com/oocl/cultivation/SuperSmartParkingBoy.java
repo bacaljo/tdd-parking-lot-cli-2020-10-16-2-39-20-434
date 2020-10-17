1f5592d6b09be104bfbc8c42f3f501af541b0faa
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-17 14:38:17 +0800

    [Story 5] [John Emmanuel] Feature: Override park in SuperSmartParkingBoy and implement the logic to park the given car in the parking lot with the largest available position rate

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,5 +1,7 @@
 package com.oocl.cultivation;
 
+import com.oocl.cultivation.exception.FullParkingException;
+
 import java.util.List;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
@@ -9,5 +11,16 @@ public class SuperSmartParkingBoy extends ParkingBoy {
 
     public SuperSmartParkingBoy(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
+    }
+
+    public ParkingTicket park(Car car) {
+        ParkingLot parkingLot = parkingLotList.stream()
+                .reduce((largestAvailablePositionRateParkingLot, p) ->
+                        ((double) p.countEmptyPositions() / (double) p.getCapacity() > (double) largestAvailablePositionRateParkingLot.countEmptyPositions() / (double) largestAvailablePositionRateParkingLot.getCapacity())
+                                ? p
+                                : largestAvailablePositionRateParkingLot)
+                .orElseThrow(FullParkingException::new);
+
+        return parkingLot.park(car);
     }
 }
