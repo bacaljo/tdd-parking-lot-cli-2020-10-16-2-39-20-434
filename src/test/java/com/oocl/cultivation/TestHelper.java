9721454949a278b1de94c38506d990a63a3d8cdc
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 19:35:39 +0800

    [Strategy] [John Emmanuel] Refactor: Remove unused constants

--- src/test/java/com/oocl/cultivation/TestHelper.java
+++ src/test/java/com/oocl/cultivation/TestHelper.java
@@ -1,8 +1,6 @@
 package com.oocl.cultivation;
 
 import com.oocl.cultivation.factory.ParkingBoyFactory;
-import com.oocl.cultivation.strategy.ParkingStrategy;
-import com.oocl.cultivation.strategy.parking.SequentialParkingStrategy;
 
 import java.util.List;
 
@@ -10,12 +8,10 @@ public final class TestHelper {
     public static final String UNRECOGNIZED_PARKING_TICKET_EXCEPTION_MESSAGE = "Unrecognized parking ticket.";
     public static final String MISSING_PARKING_TICKET_EXCEPTION_MESSAGE = "Please provide your parking ticket.";
     public static final String FULL_PARKING_EXCEPTION_MESSAGE = "Not enough position.";
-    public static final String PARKING_BOY_MANAGEMENT_EXCEPTION_MESSAGE = "The parking boy is not under your management.";
     public static final String INVALID_PARKING_EXCEPTION_MESSAGE = "Car is already parked.";
     public static final int FIRST_ELEMENT = 0;
     public static final int SECOND_ELEMENT = 1;
     public static final int THIRD_ELEMENT = 2;
-    public static final ParkingStrategy SEQUENTIAL_PARKING_STRATEGY = new SequentialParkingStrategy();
 
     private TestHelper() {
     }
