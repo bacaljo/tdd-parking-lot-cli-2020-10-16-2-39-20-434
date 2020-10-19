35cea9e152f5d4f32f89c29386db8d228162fce4
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 11:09:13 +0800

    [Strategy] [John Emmanuel] Test: Test ParkingBoyFactory.getParkingBoy if it will return a ParkingBoy that uses LargestAvailableRateParkingStrategy given ParkingBoyType.SUPER_SMART_PARKING_BOY

--- src/test/java/com/oocl/cultivation/factory/ParkingBoyFactoryTest.java
+++ src/test/java/com/oocl/cultivation/factory/ParkingBoyFactoryTest.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation.factory;
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingBoyType;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.strategy.parking.LargestAvailableRateParkingStrategy;
 import com.oocl.cultivation.strategy.parking.MostEmptyParkingStrategy;
 import com.oocl.cultivation.strategy.parking.SequentialParkingStrategy;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import java.util.List;
 
 import static com.oocl.cultivation.ParkingBoyType.PARKING_BOY;
 import static com.oocl.cultivation.ParkingBoyType.SMART_PARKING_BOY;
+import static com.oocl.cultivation.ParkingBoyType.SUPER_SMART_PARKING_BOY;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -49,6 +51,24 @@ class ParkingBoyFactoryTest {
 
         // then
         assertTrue(parkingBoy.getParkingStrategy() instanceof MostEmptyParkingStrategy);
+        assertEquals(expectedParkingLotCount, parkingBoy.getParkingLotList().size());
+    }
+
+    @Test
+    public void should_return_a_parking_boy_with_largest_available_rate_parking_strategy_and_a_list_of_parking_lots_with_3_elements_when_get_parking_boy_given_super_smart_parking_boy_and_a_list_of_parking_lots_with_3_elements() {
+        // given
+        ParkingBoyFactory parkingBoyFactory = new ParkingBoyFactory();
+        List<ParkingLot> parkingLotList = asList(
+                new ParkingLot(), new ParkingLot(), new ParkingLot());
+        ParkingBoyType parkingBoyType = SUPER_SMART_PARKING_BOY;
+
+        int expectedParkingLotCount = 3;
+
+        // when
+        ParkingBoy parkingBoy = parkingBoyFactory.getParkingBoy(parkingBoyType, parkingLotList);
+
+        // then
+        assertTrue(parkingBoy.getParkingStrategy() instanceof LargestAvailableRateParkingStrategy);
         assertEquals(expectedParkingLotCount, parkingBoy.getParkingLotList().size());
     }
 }
