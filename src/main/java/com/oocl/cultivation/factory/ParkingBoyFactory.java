17b486f2c507119097d6e3118b6f66470177849f
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 11:07:36 +0800

    [Strategy] [John Emmanuel] Feature: Return a ParkingBoy that uses MostEmptyParkingStrategy in ParkingBoyFactory.getParkingBoy when given ParkingBoyType.SMART_PARKING_BOY

--- src/main/java/com/oocl/cultivation/factory/ParkingBoyFactory.java
+++ src/main/java/com/oocl/cultivation/factory/ParkingBoyFactory.java
@@ -3,11 +3,13 @@ package com.oocl.cultivation.factory;
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingBoyType;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.strategy.parking.MostEmptyParkingStrategy;
 import com.oocl.cultivation.strategy.parking.SequentialParkingStrategy;
 
 import java.util.List;
 
 import static com.oocl.cultivation.ParkingBoyType.PARKING_BOY;
+import static com.oocl.cultivation.ParkingBoyType.SMART_PARKING_BOY;
 
 public class ParkingBoyFactory {
     public ParkingBoyFactory() {
@@ -16,6 +18,8 @@ public class ParkingBoyFactory {
     public ParkingBoy getParkingBoy(ParkingBoyType parkingBoyType, List<ParkingLot> parkingLotList) {
         if (parkingBoyType == PARKING_BOY) {
             return new ParkingBoy(new SequentialParkingStrategy(), parkingLotList);
+        } else if (parkingBoyType == SMART_PARKING_BOY) {
+            return new ParkingBoy(new MostEmptyParkingStrategy(), parkingLotList);
         }
         return null;
     }
