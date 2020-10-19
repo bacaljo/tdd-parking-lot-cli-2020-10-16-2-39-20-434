dfa8f391f28c44ccc5593a72202a1ac34ee982d8
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 11:10:00 +0800

    [Strategy] [John Emmanuel] Feature: Return a ParkingBoy that uses LargestAvailableRateParkingStrategy in ParkingBoyFactory.getParkingBoy when given ParkingBoyType.SUPER_SMART_PARKING_BOY

--- src/main/java/com/oocl/cultivation/factory/ParkingBoyFactory.java
+++ src/main/java/com/oocl/cultivation/factory/ParkingBoyFactory.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation.factory;
 import com.oocl.cultivation.ParkingBoy;
 import com.oocl.cultivation.ParkingBoyType;
 import com.oocl.cultivation.ParkingLot;
+import com.oocl.cultivation.strategy.parking.LargestAvailableRateParkingStrategy;
 import com.oocl.cultivation.strategy.parking.MostEmptyParkingStrategy;
 import com.oocl.cultivation.strategy.parking.SequentialParkingStrategy;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 import static com.oocl.cultivation.ParkingBoyType.PARKING_BOY;
 import static com.oocl.cultivation.ParkingBoyType.SMART_PARKING_BOY;
+import static com.oocl.cultivation.ParkingBoyType.SUPER_SMART_PARKING_BOY;
 
 public class ParkingBoyFactory {
     public ParkingBoyFactory() {
@@ -20,6 +22,8 @@ public class ParkingBoyFactory {
             return new ParkingBoy(new SequentialParkingStrategy(), parkingLotList);
         } else if (parkingBoyType == SMART_PARKING_BOY) {
             return new ParkingBoy(new MostEmptyParkingStrategy(), parkingLotList);
+        } else if (parkingBoyType == SUPER_SMART_PARKING_BOY) {
+            return new ParkingBoy(new LargestAvailableRateParkingStrategy(), parkingLotList);
         }
         return null;
     }
