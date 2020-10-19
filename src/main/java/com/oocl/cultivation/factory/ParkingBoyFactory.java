99afae4624234262eef735f333bb7135d3fef466
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 16:56:27 +0800

    [Strategy] [John Emmanuel] Refactor: Change importing of ParkingBoyType(s) to static

--- src/main/java/com/oocl/cultivation/factory/ParkingBoyFactory.java
+++ src/main/java/com/oocl/cultivation/factory/ParkingBoyFactory.java
@@ -10,13 +10,17 @@ import com.oocl.cultivation.strategy.parking.SequentialParkingStrategy;
 
 import java.util.List;
 
+import static com.oocl.cultivation.ParkingBoyType.PARKING_BOY;
+import static com.oocl.cultivation.ParkingBoyType.SMART_PARKING_BOY;
+import static com.oocl.cultivation.ParkingBoyType.SUPER_SMART_PARKING_BOY;
+
 public class ParkingBoyFactory {
     public ParkingBoy getParkingBoy(ParkingBoyType parkingBoyType, List<ParkingLot> parkingLotList) {
-        if (parkingBoyType == ParkingBoyType.PARKING_BOY) {
+        if (parkingBoyType == PARKING_BOY) {
             return new ParkingBoy(new SequentialParkingStrategy(), parkingLotList);
-        } else if (parkingBoyType == ParkingBoyType.SMART_PARKING_BOY) {
+        } else if (parkingBoyType == SMART_PARKING_BOY) {
             return new ParkingBoy(new MostEmptyParkingStrategy(), parkingLotList);
-        } else if (parkingBoyType == ParkingBoyType.SUPER_SMART_PARKING_BOY) {
+        } else if (parkingBoyType == SUPER_SMART_PARKING_BOY) {
             return new ParkingBoy(new LargestAvailableRateParkingStrategy(), parkingLotList);
         }
         throw new UnsupportedParkingBoyTypeException();
